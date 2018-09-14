73c25d0dde864c5212890b2df0d32fcafa1787e4
Augusto da Silva Costa <contato@guto.pro.br>
2018-09-14 18:21:16 -0300

    Os arquivos da solução

---
+++ udacity/faca_uma_parada_test.py
@@ -0,0 +1,10 @@
+import unittest
+from faca_uma_parada import webbrowser.open
+
+class TesteUm(unittest.TestCase):
+
+    def setUp(self):
+        pass
+	
+if __name__ == '__main__':
+    unittest.main()        
